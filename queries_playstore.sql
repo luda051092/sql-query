e61b3c61caa6274575a0ac8290488d10367c62ec
luda051092 <78949655+luda051092@users.noreply.github.com>
2023-08-14 20:01:53 -0400

    Add files via upload

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -1,1 +1,139 @@
--- Comments in SQL Start with dash-dash --
+--1. Find entire record for the app with an ID of '1880'. 
+SELECT * FROM analytics 
+    WHERE id = 1880;
+
+--2. Find ID and app name for all apps last updated on Aug. 1, 2018. 
+SELECT id, app_name FROM analytics
+    WHERE last_updated = '2018-08-01';
+
+--3. Count the number of apps in each category. 
+
+SELECT category, COUNT(*) FROM analytics
+    GROUP BY category;
+
+--4. Find the top 5 most-reviewed apps and the number of reviews for each. 
+SELECT * FROM analytics
+    ORDER BY reviews DESC
+    LIMIT 5;
+
+--5. Find the full record of the app that has the most reviews
+--     with a rating greater or equal to 4.8.
+
+SELECT * FROM analytics
+    WHERE rating >= 4.8
+    ORDER BY reviews DESC
+    LIMIT 1;
+
+-- 6. Find avg rating for each category ordered 
+--      by the highest rated to lowest rated.
+SELECT category, AVG(rating) FROM analytics
+    GROUP BY category
+    ORDER BY avg DESC;
+
+--7. Find name, price, and rating of most expensive app with a rating less than 3. 
+
+SELECT app_name, price, rating FROM analytics
+    WHERE rating < 3 
+    ORDER BY price DESC
+    LIMIT 1;
+
+--8. Find all records with a min install not exceeding 50, that have a rating. 
+--    order results by highest rated first. 
+SELECT * FROM analytics
+    WHERE min_installs <= 50
+        AND rating IS NOT NULL
+    ORDER BY rating DESC;
+
+--9. Find names of all apps rated less than 3 with at least 1000 reviews. 
+SELECT app_name FROM analytics
+    WHERE rating < 3 AND reviews >= 10000;
+
+--10. Find top 10 most reviewed apps that cost between 10 cents and 1.00.
+SELECT * FROM analytics
+    WHERE price BETWEEN 0.1 and 1
+    ORDER BY reviews DESC
+    LIMIT 10; 
+
+--11. Find most out of date app. 
+SELECT * FROM analytics
+    ORDER BY last_updated LIMIT 1;
+
+--12. Find most expensive app. 
+SELECT * FROM analytics
+    ORDER BY price DESC LIMIT 1;
+
+--13. Count all reviews in the Google Play Store. 
+SELECT SUM(reviews) AS "All the Reviews" FROM analytics;
+
+--14. Find all categories that have more than 300 apps in them.
+SELECT category FROM analytics
+    GROUP BY category
+    HAVING COUNT(*) > 300;
+
+--15. Find the app that has the highest proportion of reviews to min_installs,
+-- among apps that have been installed at least 100,000 times. Display the name of the app
+-- along with the number of reviews, the min_installs, and the proportion
+SELECT app_name, reviews, min_installs, min_installs / reviews AS proportion
+    FROM analytics
+    WHERE min_installs >= 100000
+    ORDER BY proportion DESC
+    LIMIT 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- queries_products.sql
+++ queries_products.sql
@@ -1,1 +1,72 @@
--- Comments in SQL Start with dash-dash --
+-- Add a product to the table with name of "chair", 
+-- price of 44.00, and can_be_returned of false. 
+
+
+INSERT INTO products
+    (name, price, can_be_returned)
+VALUES
+    ('chair', 44.00, 'f');
+
+-- Add a product to table with name of "stool", 
+-- price of 25.99, and can_be_returned of true. 
+
+INSERT INTO products
+    (name, price, can_be_returned)
+VALUES
+    ('stool', 25.99, 't');
+
+-- Add a product to table with name of "table" 
+-- price of 124.00 and can_be_returned of false. 
+
+INSERT INTO products
+    (name, price, can_be_returned)
+VALUES
+    ('table', 124.00, 'f');
+
+-- Display all the rows and columns in the table. 
+
+SELECT * FROM products;
+
+--Display all names of products
+
+SELECT name FROM products;
+
+--Display all names and prices of products
+
+SELECT name, price FROM products;
+
+-- Add a new product
+
+INSERT INTO products
+    (name, price, can_be_returned)
+VALUES
+    ('lamp', 44.99, 'f');
+
+-- Display only products that can_be_returned
+
+SELECT * FROM products WHERE can_be_returned;
+
+-- Display only products that have a price less than 44.00
+
+SELECT * FROM products WHERE price < 44.00;
+
+-- Display only products that have a price less between 22.50 and 99.99
+
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99; 
+
+-- There is a sale going on. Everything is 20.00 off. Update the database accordingly. 
+
+UPDATE products SET price = price - 20;
+
+-- Because of the sale, everything less than $25 has sold out. 
+-- Remove all products whose price meets this criteria. 
+
+DELETE FROM products WHERE price < 25;
+
+-- The sale is over. For remaining products, increase the price by $20. 
+
+UPDATE products SET price = price + 20;
+
+-- Change in company policy, all products now returnable
+
+UPDATE products SET can_be_returned = 't';
